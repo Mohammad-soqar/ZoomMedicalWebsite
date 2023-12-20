e313feec9ab9eb828018649f8f0f4421f3f0b2ea
Mohammad Soqar <mnsoqar1@gmail.com>
2023-12-20 20:04:48 +0300

    languages

--- assets/js/translation.js
+++ assets/js/translation.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     function updateLanguageLinks() {
         var path = window.location.pathname;
-        var page = path.substring(path.lastIndexOf('/') + 1);
+        var pathParts = path.split('/').filter(function(part) { return part !== ''; });
+        
+        // Assuming the language code is always the first part of the path
+        var currentPage = pathParts.slice(1).join('/'); // Remove the language part
 
         var languages = {
-            'en': '/en/' + page,
-            'ar': '/ar/' + page,
-            'fr': '/fr/' + page,
+            'en': '/en/' + currentPage,
+            'ar': '/ar/' + currentPage,
+            'fr': '/fr/' + currentPage,
             // Add more languages as needed
         };
 
