0be69100ac4fbf83c10373a8523652b6e3b0f342
Mohammed Alazaar <mhmdalazr@icloud.com>
2023-12-24 22:56:22 +0300

    home page done

--- assets/js/indix.js
+++ assets/js/indix.js
@@ -14,3 +14,18 @@ $(document).ready(function() {
         setTimeout(showSlides, 3000); // Change slide every 3 seconds
     }
 });
+
+
+
+$(document).ready(function () {
+    const $viewport = $(".carousel-viewport");
+    const $cardContainers = $(".review-card-container");
+    const cardContainerWidth = $cardContainers.first().outerWidth(); // Get the width of one card container
+    let currentIndex = 0;
+  
+    $(".next-btn").on("click", function () {
+      currentIndex = (currentIndex + 1) % $cardContainers.length;
+      const translateX = -currentIndex * cardContainerWidth;
+      $viewport.css("transform", `translateX(${translateX}px)`);
+    });
+  });
