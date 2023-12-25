9769cd3c4be14571a39705b18e603075978dd51b
Mohammed Alazaar <mhmdalazr@icloud.com>
2023-12-25 20:04:47 +0300

    home page after some edits

--- assets/js/indix.js
+++ assets/js/indix.js
@@ -1,31 +1,14 @@
-$(document).ready(function() {
-    var slideIndex = 0;
-    showSlides();
+$(document).ready(function(){
+    var currentCardIndex = 0;
+    var totalCards = $('.review-card-container').length;
 
-    function showSlides() {
-        var i;
-        var slides = $(".slideshow-container-mySlides");
-        for (i = 0; i < slides.length; i++) {
-            slides.eq(i).hide(); // Hide all slides
+    $('.next-btn').click(function(){
+        if (currentCardIndex < totalCards - 1) {
+            currentCardIndex++;
+        } else {
+            currentCardIndex = 0;
         }
-        slideIndex++;
-        if (slideIndex > slides.length) { slideIndex = 1 }
-        slides.eq(slideIndex - 1).fadeIn(1000); // Fade in the current slide
-        setTimeout(showSlides, 3000); // Change slide every 3 seconds
-    }
-});
-
-
-
-$(document).ready(function () {
-    const $viewport = $(".carousel-viewport");
-    const $cardContainers = $(".review-card-container");
-    const cardContainerWidth = $cardContainers.first().outerWidth(); // Get the width of one card container
-    let currentIndex = 0;
-  
-    $(".next-btn").on("click", function () {
-      currentIndex = (currentIndex + 1) % $cardContainers.length;
-      const translateX = -currentIndex * cardContainerWidth;
-      $viewport.css("transform", `translateX(${translateX}px)`);
+        var newMargin = -currentCardIndex * 100; // Assuming 100% width per card
+        $('.carousel-viewport').animate({marginLeft: newMargin + '%'}, 500);
     });
-  });
+});
